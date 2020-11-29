a774c7f20e31e1c99840ab929ac016f9b100b832
jessda <37726333+jessda@users.noreply.github.com>
2020-11-29 19:51:37 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    Timer += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    Timer = 0
+    Countdown = false
+})
+input.onButtonPressed(Button.B, function () {
+    Countdown = true
+})
+let Countdown = false
+let Timer = 0
+Timer = 0
+Countdown = false
+basic.forever(function () {
+    basic.showNumber(Timer)
+    if (Countdown == true) {
+        basic.pause(1000)
+        Timer += -1
+        if (Timer <= 0) {
+            Countdown = false
+        }
+    }
+})
